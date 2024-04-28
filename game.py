9c327251f188dd6185586d45c6f7f0efb9b7ab2f
OlekCydejko <olekcydejko98@gmail.com>
2024-04-28 11:11:45 +0200

    first try sequence

--- game.py
+++ game.py
@@ -2,16 +2,24 @@ import os
 from time import sleep
 import random
 
-zmienna = [1, 2, 3]
-x = input("cos tam: ")
-print(x)
-sleep(1)
-os.system('cls')
-
 def clear():
     if os.name == "posix":
         os.system("clear")
     else:
         os.system("cls")
 
+main_sequence = []
+main_sequence.append(str(random.randint(1, 4)))
+print(main_sequence)
+sleep(3)
 clear()
+user_sequence = input("Wprowadź poprzednią sekwęcję")
+join_sequence = "".join(main_sequence)
+if user_sequence == join_sequence:
+    main_sequence.append(str(random.randint(1, 4)))
+else:
+    print("Odpowiedź błędna")
+
+print(main_sequence)
+sleep(1)
+
